cd058d47d88d13ca12a21d0b4643104f864198e4
liiz-durang <lizeth.durang@gmail.com>
2024-08-13 23:36:40 -0600

    maximumFractionDigits: 0 -> maximumFractionDigits: 2

--- src/utils/formatNumber.ts
+++ src/utils/formatNumber.ts
@@ -2,6 +2,6 @@ export const formatNumber = (num?: number) =>
   !Number.isNaN(num)
     ? Number(num ?? 0).toLocaleString("en-US", {
         minimumFractionDigits: 0,
-        maximumFractionDigits: 0,
+        maximumFractionDigits: 2,
       }) ?? "0"
     : "0";
