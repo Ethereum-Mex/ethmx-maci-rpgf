c5607f3a2c4cc14eb39a72dd84e3eb1f08828c0b
Liz Durán <42351387+liiz-durang@users.noreply.github.com>
2024-08-27 14:11:59 -0600

    Update Projects.tsx
    no mostrar votos

--- src/features/projects/components/Projects.tsx
+++ src/features/projects/components/Projects.tsx
@@ -76,11 +76,13 @@ export function Projects() {
                   />
                 </div>
               ) : null}
+              {/** 
               {!results.isLoading && appState === EAppState.RESULTS ? (
                 <ProjectItemAwarded
                   amount={results.data?.projects?.[item.id]?.votes}
                 />
               ) : null}
+              */}
               <ProjectItem isLoading={isLoading} attestation={item} />
             </Link>
           );
