04bf1b8f581cd46f8af397b072d93015d48a778f
liiz-durang <lizeth.durang@gmail.com>
2024-08-30 13:12:52 -0600

    ver resultados sin hacer login

--- src/pages/stats/index.tsx
+++ src/pages/stats/index.tsx
@@ -23,24 +23,13 @@ const ResultsChart = dynamic(
 );
 
 export default function StatsPage() {
-  const appState = getAppState();
-  const duration = differenceInDays(config.resultsAt, new Date());
-
   return (
     <Layout>
       <Heading as="h1" size="3xl">
-        Resultados preliminares de la ronda #1 Ethereum México PGF 
+        Resultados preliminares de la ronda #1 Ethereum México PGF
       </Heading>
 
-      {appState === EAppState.RESULTS ? (
-        <Stats />
-      ) : (
-        <Alert variant="info" className="mx-auto max-w-sm text-center">
-          Los resultados serán revelados en {" "}
-          <div className="text-3xl">{duration > 0 ? duration : 0}</div>
-          días
-        </Alert>
-      )}
+      <Stats />
     </Layout>
   );
 }
@@ -51,8 +40,9 @@ function Stats() {
   const count = useProjectCount();
   const { data: projectsResults } = useProjectsResults(pollData);
   const { isConnected } = useAccount();
-
+  const appState = getAppState();
   const [isScriptLoaded, setIsScriptLoaded] = useState(false);
+  const duration = differenceInDays(config.resultsAt, new Date());
 
   const { averageVotes, projects = {} } = results.data ?? {};
 
@@ -70,7 +60,7 @@ function Stats() {
   useEffect(() => {
     const loadScript = () => {
       if (!isScriptLoaded) {
-        const script = document.createElement('script');
+        const script = document.createElement("script");
         script.src = "https://public.flourish.studio/resources/embed.js";
         script.async = true;
         document.body.appendChild(script);
@@ -88,88 +78,60 @@ function Stats() {
 
     // Vuelve a cargar el script cuando el componente se vuelve a mostrar
     const handleVisibilityChange = () => {
-      if (document.visibilityState === 'visible') {
+      if (document.visibilityState === "visible") {
         loadScript();
       }
     };
 
-    document.addEventListener('visibilitychange', handleVisibilityChange);
-    return () => document.removeEventListener('visibilitychange', handleVisibilityChange);
+    document.addEventListener("visibilitychange", handleVisibilityChange);
+    return () =>
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
   }, [isScriptLoaded]);
 
   if (isLoading) {
     return <div>Cargando...</div>;
   }
 
-  if (!pollData && !isConnected) {
-    return (
-      <Alert variant="info" className="mx-auto max-w-sm text-center">
-        <h3 className="text-lg font-bold">
-          Conecta tu wallet para ver los resultados
-        </h3>
-
-        <div className="mt-4">
-          <ConnectButton />
-        </div>
-      </Alert>
-    );
-  }
-
-  if (!pollData) {
-    return <div>Algo salió mal. Intenta de nuevo.</div>;
-  }
-
-  return (
+  return /*appState === EAppState.RESULTS ?]*/ (
     <>
       <div>
         <br />
         <h3 className="text-lg">
-          En los próximos días compartiremos un análisis más detallado de los resultados.
+          En los próximos días compartiremos un análisis más detallado de los
+          resultados.
         </h3>
-        <a 
-          style={{ textDecoration: 'underline' }} 
+        <a
+          style={{ textDecoration: "underline" }}
           href="https://docs.google.com/spreadsheets/d/18KNgbFJRwynSWX5wXRTxSxzLLoj5Rybcq1Yg3q0DOAo/edit?gid=663021552#gid=663021552"
           target="_blank"
           rel="noopener noreferrer"
         >
-        Consulta la información adicional aquí.
+          Consulta la información adicional aquí.
         </a>
         <br /> <br />
         <p className="text-lg">¡Muchas gracias por participar!</p>
         <br />
 
-        <div className="flourish-embed flourish-hierarchy" data-src="visualisation/19217853">
+        <div
+          className="flourish-embed flourish-hierarchy"
+          data-src="visualisation/19217853"
+        >
           <noscript>
-            <img 
-              src="https://public.flourish.studio/visualisation/19217853/thumbnail" 
-              width="100%" 
-              alt="hierarchy visualization" 
+            <img
+              src="https://public.flourish.studio/visualisation/19217853/thumbnail"
+              width="100%"
+              alt="hierarchy visualization"
             />
           </noscript>
         </div>
       </div>
-
-      <div>
-        {/* 
-          <h3 className="text-lg font-bold">Proyectos Principales</h3>
-          <div className="mb-8 h-[400px] rounded-xl bg-white text-black">
-            <ResultsChart data={chartData} />
-          </div>
-
-          <div className="grid gap-2 md:grid-cols-3">
-            <Stat title="Proyectos Participantes">{count.data?.count}</Stat>
-            <Stat title="Proyectos Votados ">{Object.keys(projects).length}</Stat>
-            <Stat title="Votantes">
-              {pollData?.numSignups ? Number(pollData?.numSignups) - 1 : 0}
-            </Stat>
-            <Stat title="Votos Promedio Por Proyecto">
-              {formatNumber(averageVotes)}
-            </Stat>
-          </div>
-        */}
-      </div>
     </>
-  );
+  )/* : (
+    <Alert variant="info" className="mx-auto max-w-sm text-center">
+      Los resultados serán revelados en{" "}
+      <div className="text-3xl">{duration > 0 ? duration : 0}</div> días
+    </Alert>
+  );*/
 }
 
 function Stat({ title, children }: PropsWithChildren<{ title: string }>) {
